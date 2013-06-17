6d4b27b039cfdc8031a9caf6584dfe846c0ee795
victormb <victor--mb@hotmail.com>
2013-06-16 21:55:14 -0300

    Create Operacoes.Designer.cs

---
+++ Operacoes.Designer.cs
@@ -0,0 +1,403 @@
+namespace Conjunto
+{
+    partial class FormOperacoesConjuntos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxElemento = new System.Windows.Forms.TextBox();
+            this.textBoxAdd = new System.Windows.Forms.TextBox();
+            this.buttonPertence = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelElementoComparar = new System.Windows.Forms.Label();
+            this.buttonAddConjA = new System.Windows.Forms.Button();
+            this.textBoxConjA = new System.Windows.Forms.TextBox();
+            this.buttonContem = new System.Windows.Forms.Button();
+            this.Pertinecia = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.buttonProd = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.btnHeapSort = new System.Windows.Forms.Button();
+            this.groupBoxEndorrelacao = new System.Windows.Forms.GroupBox();
+            this.buttonEndorrelacao = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBoxaSubconjo = new System.Windows.Forms.GroupBox();
+            this.Pertinecia.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBoxEndorrelacao.SuspendLayout();
+            this.groupBoxaSubconjo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxElemento
+            // 
+            this.textBoxElemento.Location = new System.Drawing.Point(36, 37);
+            this.textBoxElemento.Name = "textBoxElemento";
+            this.textBoxElemento.Size = new System.Drawing.Size(100, 20);
+            this.textBoxElemento.TabIndex = 0;
+            this.textBoxElemento.TextChanged += new System.EventHandler(this.textBoxElemento_TextChanged);
+            // 
+            // textBoxAdd
+            // 
+            this.textBoxAdd.Location = new System.Drawing.Point(143, 32);
+            this.textBoxAdd.Name = "textBoxAdd";
+            this.textBoxAdd.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAdd.TabIndex = 0;
+            this.textBoxAdd.TextChanged += new System.EventHandler(this.textBoxAdd_TextChanged);
+            // 
+            // buttonPertence
+            // 
+            this.buttonPertence.Location = new System.Drawing.Point(24, 63);
+            this.buttonPertence.Name = "buttonPertence";
+            this.buttonPertence.Size = new System.Drawing.Size(121, 23);
+            this.buttonPertence.TabIndex = 2;
+            this.buttonPertence.Text = "Pertence ao Conj. N?";
+            this.buttonPertence.UseVisualStyleBackColor = true;
+            this.buttonPertence.Click += new System.EventHandler(this.buttonPertence_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Elementos do Conjunto N";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(154, 58);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Adicionar";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelElementoComparar
+            // 
+            this.labelElementoComparar.AutoSize = true;
+            this.labelElementoComparar.Location = new System.Drawing.Point(33, 19);
+            this.labelElementoComparar.Name = "labelElementoComparar";
+            this.labelElementoComparar.Size = new System.Drawing.Size(101, 13);
+            this.labelElementoComparar.TabIndex = 5;
+            this.labelElementoComparar.Text = "Elemento a Verificar";
+            // 
+            // buttonAddConjA
+            // 
+            this.buttonAddConjA.Location = new System.Drawing.Point(509, 58);
+            this.buttonAddConjA.Name = "buttonAddConjA";
+            this.buttonAddConjA.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddConjA.TabIndex = 8;
+            this.buttonAddConjA.Text = "Adicionar";
+            this.buttonAddConjA.UseVisualStyleBackColor = true;
+            this.buttonAddConjA.Click += new System.EventHandler(this.buttonAddConjA_Click);
+            // 
+            // textBoxConjA
+            // 
+            this.textBoxConjA.Location = new System.Drawing.Point(497, 32);
+            this.textBoxConjA.Name = "textBoxConjA";
+            this.textBoxConjA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxConjA.TabIndex = 6;
+            this.textBoxConjA.TextChanged += new System.EventHandler(this.textBoxConjA_TextChanged);
+            // 
+            // buttonContem
+            // 
+            this.buttonContem.Location = new System.Drawing.Point(19, 43);
+            this.buttonContem.Name = "buttonContem";
+            this.buttonContem.Size = new System.Drawing.Size(160, 23);
+            this.buttonContem.TabIndex = 10;
+            this.buttonContem.Text = " A Contém em N ?";
+            this.buttonContem.UseVisualStyleBackColor = true;
+            this.buttonContem.Click += new System.EventHandler(this.buttonContem_Click);
+            // 
+            // Pertinecia
+            // 
+            this.Pertinecia.Controls.Add(this.textBoxElemento);
+            this.Pertinecia.Controls.Add(this.buttonPertence);
+            this.Pertinecia.Controls.Add(this.labelElementoComparar);
+            this.Pertinecia.Location = new System.Drawing.Point(30, 90);
+            this.Pertinecia.Name = "Pertinecia";
+            this.Pertinecia.Size = new System.Drawing.Size(200, 109);
+            this.Pertinecia.TabIndex = 11;
+            this.Pertinecia.TabStop = false;
+            this.Pertinecia.Text = "Pertinencia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Elementos do Conjunto A";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonContem);
+            this.groupBox1.Location = new System.Drawing.Point(259, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Continência";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(484, 99);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "União";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "União";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Intersecção";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(30, 233);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Intersecção";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Diferença";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Location = new System.Drawing.Point(259, 233);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 100);
+            this.groupBox4.TabIndex = 16;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Diferença";
+            // 
+            // buttonProd
+            // 
+            this.buttonProd.Location = new System.Drawing.Point(36, 44);
+            this.buttonProd.Name = "buttonProd";
+            this.buttonProd.Size = new System.Drawing.Size(121, 23);
+            this.buttonProd.TabIndex = 6;
+            this.buttonProd.Text = "N x A";
+            this.buttonProd.UseVisualStyleBackColor = true;
+            this.buttonProd.Click += new System.EventHandler(this.buttonProd_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.buttonProd);
+            this.groupBox5.Location = new System.Drawing.Point(484, 233);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 100);
+            this.groupBox5.TabIndex = 17;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Produto";
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Location = new System.Drawing.Point(295, 458);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(140, 26);
+            this.buttonLimpar.TabIndex = 18;
+            this.buttonLimpar.Text = "Limpar Conjuntos";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.btnHeapSort);
+            this.groupBox6.Location = new System.Drawing.Point(259, 352);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(200, 100);
+            this.groupBox6.TabIndex = 19;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Produto";
+            // 
+            // btnHeapSort
+            // 
+            this.btnHeapSort.Location = new System.Drawing.Point(46, 48);
+            this.btnHeapSort.Name = "btnHeapSort";
+            this.btnHeapSort.Size = new System.Drawing.Size(121, 23);
+            this.btnHeapSort.TabIndex = 6;
+            this.btnHeapSort.Text = "HeapSort";
+            this.btnHeapSort.UseVisualStyleBackColor = true;
+            this.btnHeapSort.Click += new System.EventHandler(this.btnHeapSort_Click);
+            // 
+            // groupBoxEndorrelacao
+            // 
+            this.groupBoxEndorrelacao.Controls.Add(this.buttonEndorrelacao);
+            this.groupBoxEndorrelacao.Location = new System.Drawing.Point(487, 352);
+            this.groupBoxEndorrelacao.Name = "groupBoxEndorrelacao";
+            this.groupBoxEndorrelacao.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxEndorrelacao.TabIndex = 20;
+            this.groupBoxEndorrelacao.TabStop = false;
+            this.groupBoxEndorrelacao.Text = "Endorrelação";
+            // 
+            // buttonEndorrelacao
+            // 
+            this.buttonEndorrelacao.Location = new System.Drawing.Point(33, 48);
+            this.buttonEndorrelacao.Name = "buttonEndorrelacao";
+            this.buttonEndorrelacao.Size = new System.Drawing.Size(121, 23);
+            this.buttonEndorrelacao.TabIndex = 6;
+            this.buttonEndorrelacao.Text = "Endorrelação de N";
+            this.buttonEndorrelacao.UseVisualStyleBackColor = true;
+            this.buttonEndorrelacao.Click += new System.EventHandler(this.buttonEndorrelacao_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(33, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Gerar SubConjuntos";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBoxaSubconjo
+            // 
+            this.groupBoxaSubconjo.Controls.Add(this.button4);
+            this.groupBoxaSubconjo.Location = new System.Drawing.Point(487, 466);
+            this.groupBoxaSubconjo.Name = "groupBoxaSubconjo";
+            this.groupBoxaSubconjo.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxaSubconjo.TabIndex = 21;
+            this.groupBoxaSubconjo.TabStop = false;
+            this.groupBoxaSubconjo.Text = "Subconjuntos";
+            // 
+            // FormOperacoesConjuntos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 578);
+            this.Controls.Add(this.groupBoxaSubconjo);
+            this.Controls.Add(this.groupBoxEndorrelacao);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAdd);
+            this.Controls.Add(this.buttonAddConjA);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxConjA);
+            this.Controls.Add(this.Pertinecia);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormOperacoesConjuntos";
+            this.Text = "Operações entre Conjuntos";
+            this.Load += new System.EventHandler(this.FormOperacoesConjuntos_Load);
+            this.Pertinecia.ResumeLayout(false);
+            this.Pertinecia.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBoxEndorrelacao.ResumeLayout(false);
+            this.groupBoxaSubconjo.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxElemento;
+        private System.Windows.Forms.TextBox textBoxAdd;
+        private System.Windows.Forms.Button buttonPertence;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelElementoComparar;
+        private System.Windows.Forms.Button buttonAddConjA;
+        private System.Windows.Forms.TextBox textBoxConjA;
+        private System.Windows.Forms.Button buttonContem;
+        private System.Windows.Forms.GroupBox Pertinecia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button buttonProd;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button btnHeapSort;
+        private System.Windows.Forms.GroupBox groupBoxEndorrelacao;
+        private System.Windows.Forms.Button buttonEndorrelacao;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBoxaSubconjo;
+    }
+}
+
